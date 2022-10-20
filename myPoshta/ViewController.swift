de1d915d42a9d1fabce2b30e2049f0b93474ff0a
Nykyta <nikitaterentev8@gmail.com>
2022-10-20 14:24:25 +0300

    Initial Commit

---
+++ myPoshta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myPoshta
+//
+//  Created by Никита Терентьев on 20.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
